bdce38f337357bf91fa2fece31478d28fe2e2332
Chipxiang <jianxiangli1996@gmail.com>
2021-03-29 19:09:19 -0400

    A hardcode shoplist view

--- app/src/main/java/com/example/scantoshop/ui/shoplist/ShoplistFragment.java
+++ app/src/main/java/com/example/scantoshop/ui/shoplist/ShoplistFragment.java
@@ -8,6 +8,7 @@ import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
@@ -23,13 +24,13 @@ public class ShoplistFragment extends Fragment {
         shoplistViewModel =
                 ViewModelProviders.of(this).get(ShoplistViewModel.class);
         View root = inflater.inflate(R.layout.fragment_shoplist, container, false);
-        final TextView textView = root.findViewById(R.id.text_home);
-        shoplistViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
+//        final TextView textView = root.findViewById(R.id.text_home);
+//        shoplistViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+//            @Override
+//            public void onChanged(@Nullable String s) {
+//                textView.setText(s);
+//            }
+//        });
         return root;
     }
 }
