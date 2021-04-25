d2ec26030230d1408710a0f5f3d530cd03fb6430
aduyinuo <40970294+aduyinuo@users.noreply.github.com>
2021-04-25 19:58:41 -0400

    profile DAO transaction

--- app/src/main/java/com/example/scantoshop/DAO/ProfileDAO.java
+++ app/src/main/java/com/example/scantoshop/DAO/ProfileDAO.java
@@ -2,6 +2,8 @@ package com.example.scantoshop.DAO;
 import androidx.room.*;
 import com.example.scantoshop.Entity.*;
 
+import java.util.List;
+
 @Dao
 public interface ProfileDAO {
     @Insert(onConflict = OnConflictStrategy.REPLACE)
@@ -18,4 +20,12 @@ public interface ProfileDAO {
 
     @Query("SELECT * FROM profile WHERE uid == :user_name")
     public Profile[] loadProfileByName(String user_name);
+
+    @Transaction
+    @Query("SELECT * FROM Profile")
+    public List<ProfileWithPurchaseHistories> getProfileWithPurchaseHistories();
+
+    @Transaction
+    @Query("SELECT * FROM Profile")
+    public List<ProfileWithFavoriteItems> getProfileWithFavoriteItems();
 }
