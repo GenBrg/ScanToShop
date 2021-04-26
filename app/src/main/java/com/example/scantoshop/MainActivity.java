762154cb3766a3b7483633c411a3dedb3218dcfc
aduyinuo <40970294+aduyinuo@users.noreply.github.com>
2021-04-25 20:35:45 -0400

    commit before checkout

--- app/src/main/java/com/example/scantoshop/MainActivity.java
+++ app/src/main/java/com/example/scantoshop/MainActivity.java
@@ -5,6 +5,10 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import androidx.room.Room;
+import com.example.scantoshop.DAO.ProfileDAO;
+import com.example.scantoshop.Entity.Profile;
+import com.example.scantoshop.util.AppDatabase;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -31,6 +35,11 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(navView, navController);
+
+        AppDatabase db = Room.databaseBuilder(getApplicationContext(),
+                AppDatabase.class, "scan_to_shop_database").build();
+        ProfileDAO profileDao = db.profileDAO();
+        Profile[] profiles = profileDao.loadAllProfiles();
 
     }
     @Override
