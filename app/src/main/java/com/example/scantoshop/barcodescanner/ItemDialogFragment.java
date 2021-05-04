24341eae172c81d6d285adf87cbfa2474d28d671
Chipxiang <jianxiangli1996@gmail.com>
2021-05-03 20:55:24 -0400

    Implemented warnings

--- app/src/main/java/com/example/scantoshop/barcodescanner/ItemDialogFragment.java
+++ app/src/main/java/com/example/scantoshop/barcodescanner/ItemDialogFragment.java
@@ -2,6 +2,7 @@ package com.example.scantoshop.barcodescanner;
 
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Color;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 
@@ -14,12 +15,16 @@ import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
+import com.example.scantoshop.DAO.ProfileDAO;
+import com.example.scantoshop.Entity.Profile;
 import com.example.scantoshop.R;
+import com.example.scantoshop.util.AppDatabase;
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment;
 import com.squareup.picasso.Picasso;
 
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+import androidx.room.Room;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -72,20 +77,64 @@ public class ItemDialogFragment extends BottomSheetDialogFragment {
                             JSONObject foodHint = parser.getJSONArray("hints").getJSONObject(0).getJSONObject("food");
                             ((TextView) v.findViewById(R.id.scannedItemName)).setText(foodHint.getString("label"));
                             ((TextView) v.findViewById(R.id.scannedItemCategory)).setText("Category: " + foodHint.getString("category"));
+                            String contentsString = "Contents: " + foodHint.getString("foodContentsLabel").toLowerCase();
+                            ((TextView) v.findViewById(R.id.scannedItemFoddContentsLabel)).setText(contentsString);
+                            AppDatabase db = Room.databaseBuilder(getContext(),
+                                    AppDatabase.class, "MyDatabase")
+                                    .createFromAsset("database/scan2shopDB.db")
+                                    .allowMainThreadQueries().build();
+                            ProfileDAO profileDao = db.profileDAO();
+                            Profile user = profileDao.loadAllProfiles()[0];
+                            String warningString = "";
+                            StringBuilder allergens = new StringBuilder("");
+                            if(user.allergy_1){
+                                if(contentsString.contains("milk")){
+                                    allergens.append("milk, ");
+                                }
+                            }
+                            if(user.allergy_2){
+                                if(contentsString.contains("egg")){
+                                    allergens.append("egg, ");
+                                }
+                            }
+                            String allergenString = "";
+                            if(allergens.length() != 0){
+                                allergenString = "Allergens detected: " + allergens.toString().substring(0, allergens.length() - 2) + "\n";
+                            }
+                            StringBuilder nutrientsWarning = new StringBuilder("");
                             StringBuilder nutrients = new StringBuilder("Nutrients:\n");
                             Iterator<String> it = foodHint.getJSONObject("nutrients").keys();
                             while(it.hasNext()){
                                 String key = it.next();
+                                double value = foodHint.getJSONObject("nutrients").getDouble(key);
+                                if(key.toLowerCase().equals("fat") && value > user.nutrient_1){
+                                    nutrientsWarning.append("fat: " + String.format("%.2f", value) + ", ");
+                                }
+                                if(key.toLowerCase().equals("sugar") && value > user.nutrient_2){
+                                    nutrientsWarning.append("sugar: " + String.format("%.2f", value) + ", ");
+                                }
                                 nutrients.append(key.substring(0, 1)).append(key.substring(1).toLowerCase())
                                         .append(": ")
-                                        .append(String.format("%.2f", foodHint.getJSONObject("nutrients").getDouble(key))).append("\n");
+                                        .append(String.format("%.2f", value)).append("; ");
                             }
+
                             ((TextView) v.findViewById(R.id.scannedItemNutrients)).setText(nutrients.toString());
+                            String nutrientsWarningString = "";
+                            if(nutrientsWarning.length() != 0){
+                                nutrientsWarningString = "Nutrients exceeds threshold: " + nutrientsWarning.toString().substring(0, nutrientsWarning.length() - 2);
+                            }
+                            if(nutrientsWarningString.length() + allergenString.length() > 0){
+                                warningString = "Warnings:\n" + allergenString+ nutrientsWarningString;
+                            }
+                            ((TextView) v.findViewById(R.id.scannedItemWarnings)).setText(warningString);
+                            ((TextView) v.findViewById(R.id.scannedItemWarnings)).setTextColor(Color.RED);
                             Picasso.with(v.getContext()).load(foodHint.getString("image")).into((ImageView)v.findViewById(R.id.scannedItemImage));
+
                         } catch (JSONException e) {
                             e.printStackTrace();
                         }
-                        rawResponseTextView.setText("Response is: " + response);
+//                        rawResponseTextView.setText("Response is: " + response);
+                        rawResponseTextView.setText("");
                     }
                 }, new Response.ErrorListener() {
             @Override
